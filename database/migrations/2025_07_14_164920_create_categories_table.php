d98a96d9af37970f6a7d8a2b1d0d0661399f0524
miku1001 <pauloted.22@gmail.com>
2025-07-15 00:58:50 +0800

    feat: Adding column in database

--- database/migrations/2025_07_14_164920_create_categories_table.php
+++ database/migrations/2025_07_14_164920_create_categories_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->timestamps();
             $table->string('name');
+            $table->string('description');
             $table->string('status')->default(1)->comment("1=visible, 0=hidden");
         });
     }
