9ac36ada16f02f3e07e9c419561bedf8664ee129
miku1001 <pauloted.22@gmail.com>
2025-07-15 02:00:10 +0800

    feat(post): Adding layout to create blade

---
+++ resources/views/category/create.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.frontend')
+
+@section('content')
+
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="card">
+          <div class="card-header">
+            <h4>Category List
+              <a href="{{url('category/create')}}" class="btn btn-primary float-end">Add Category</a>
+            </h4>
+          </div>
+          <div class="card-body">
+            <form action="{{url("category")}}" method="POST">
+              @csrf
+
+              <div class="mb-3">
+                <label for="">Name</label>
+                <input type="text" name="name" class="form-control">
+              </div>
+              <div class="mb-3">
+                <label for="">Description</label>
+                <textarea name="description" row="3" class='form-control'></textarea>
+              </div>
+              <div class="mb-3">
+                <label for="">Status</label>
+                <input type="checkbox" name="status" /> Checked=visible, unchecked=hidden
+              </div>
+              <div class="mb-3">
+                <button type="submit" class="btn btn-primary">Submit</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+
+      </div>
+    </div>
+  </div>
+
+@endsection
