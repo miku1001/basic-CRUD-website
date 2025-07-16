8f0d819f5aee6ead7884110a56574f1655f614a9
miku1001 <pauloted.22@gmail.com>
2025-07-16 18:57:54 +0800

    feat:changing app name

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -3,7 +3,7 @@
   <head>
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1">
-      <title>{{ config('app.name', 'Laravel') }}</title>
+      <title>{{ config('app.name', 'Student List') }}</title>
 
       <!-- Fonts -->
       <link rel="stylesheet" href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" />
